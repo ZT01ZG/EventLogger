989953898b1a93d8d8218c77b03db3cd65e59e85
Zachary Hagman <zach@cuddlyspork.com>
2019-11-26 06:34:39 -0800

    Initial Commit

---
+++ Spork_13_004/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spork_13_004
+//
+//  Created by Zachary Hagman on 11/26/19.
+//  Copyright © 2019 Zachary Hagman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
